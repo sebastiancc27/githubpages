0d58e86574039c794f6154bf741d4dd1e229e3b1
sebastiancc27 <sebasccastizo27@gmail.com>
2024-02-02 20:13:36 -0600

    prueba

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Esta es una prueba que mi repositorio funciona</h1>
+      <p>Se supone se debe publicar esto</p>
+    </>
+  )
+}
+
+export default App
